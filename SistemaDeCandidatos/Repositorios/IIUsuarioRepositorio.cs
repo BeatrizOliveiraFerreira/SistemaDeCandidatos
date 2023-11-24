577989cc37add09b4b784736117f494ef345ed98
Beatriz Oliveira Ferreira <66791075+BeatrizOliveiraFerreira@users.noreply.github.com>
2023-11-24 17:57:23 -0300

    Update IIUsuarioRepositorio.cs

--- SistemaDeCandidatos/Repositorios/IIUsuarioRepositorio.cs
+++ SistemaDeCandidatos/Repositorios/IIUsuarioRepositorio.cs
@@ -1,13 +1,15 @@
-﻿using SistemaDeCandidatos.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SistemaDeCandidatos.Models;
 
 namespace SistemaDeCandidatos.Repositorios
 {
     public interface IIUsuarioRepositorio
     {
         Task<UsuarioModel> Adicionar(UsuarioModel usuario);
-        Task<bool> Apagar(int id);
-        Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id);
-        Task<UsuarioModel> BuscarPorId(int id);
+        Task<bool> Apagar(int cpf);
+        Task<UsuarioModel> Atualizar(UsuarioModel usuario, int cpf);
+        Task<UsuarioModel> BuscarPorCPF(int cpf);
         Task<List<UsuarioModel>> BuscarTodosUsuarios();
     }
 }

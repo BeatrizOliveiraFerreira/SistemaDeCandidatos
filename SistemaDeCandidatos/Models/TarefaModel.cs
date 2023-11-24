fed844bbd68225f2e6f393b53ee0dbbe3e3d5343
Beatriz Oliveira Ferreira <66791075+BeatrizOliveiraFerreira@users.noreply.github.com>
2023-11-24 17:54:33 -0300

    Update TarefaModel.cs

--- SistemaDeCandidatos/Models/TarefaModel.cs
+++ SistemaDeCandidatos/Models/TarefaModel.cs
@@ -1,13 +1,12 @@
-﻿using SistemaDeCandidatos.enums;
+using SistemaDeCandidatos.enums;
 
 namespace SistemaDeCandidatos.Models
 {
     public class TarefaModel
     {
-        public int Id { get; set; }
+        public int CPF { get; set; }
         public string? Nome { get; set;}
         public string? Descricao { get; set; }
-        public int? CPF { get; set; }
         public StatusTarefa Status { get; set; }
     }
 }
